3cafef0a917d27884c6a71e6eb7bf56e61a74842
Theophilus Kibet <kibettheophilus@gmail.com>
2022-11-30 12:17:32 +0300

    Initial Commit

---
+++ src/main/kotlin/com/theophiluskibet/communityapi/CommunityApiApplication.kt
@@ -0,0 +1,11 @@
+package com.theophiluskibet.communityapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunityApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommunityApiApplication>(*args)
+}
